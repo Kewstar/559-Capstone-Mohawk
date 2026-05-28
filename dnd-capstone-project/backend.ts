a18009ef9057f481cdd1e1fa30b434630edcf72f
Kewstar <nick21292@gmail.com>
2026-05-27 20:06:16 -0400

    Commit - Initial Backend work begun - Connected my backend to supabase, can now register useres to supabase through the sign up form

---
+++ dnd-capstone-project/backend.ts
@@ -0,0 +1,58 @@
+import express from 'express';
+import cors from 'cors';
+import { log } from 'console';
+import supabase from './backend-supabase';
+
+const PORT = 8000;
+
+const app = express();
+
+app.use(cors({ origin: 'http://localhost:5173' })) 
+app.use(express.json()) 
+
+
+// —— CRUD OPERATIONS —— //
+
+//  — CREATE — // 
+app.post('/signup', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const {data, error} = await supabase.auth.signUp({
+        email: email,
+        password: password
+    });
+
+    if (error) {
+        console.error("ERROR: Could not Sign Up! ", error);
+        
+        return res.status(400).json({ message: "Signup Error ", error: error.message});
+    }
+    console.log("Authenticated User Created ", data.user?.id);
+
+    const { error: insertError } = await supabase
+        .from('users')
+        .insert({
+            id: data.user?.id,
+            username: username,
+            email: email
+        });
+    
+    if (insertError) {
+        console.error("ERROR: Could not Insert User Data: ", insertError);
+        
+        return res.status(400).json({ message: 'ERROR: User Signup Failed', error: insertError.message });
+    }
+
+    console.log("signup attempt: ", { username, email, password });
+    res.json({ message: "Signup Success!" })
+});
+
+// 
+
+// ==========
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
--- dnd-capstone-project/src/components/LoginForm/LoginForm.tsx
+++ dnd-capstone-project/src/components/LoginForm/LoginForm.tsx
@@ -24,149 +24,211 @@ export default function LoginForm() {
     };
 
 
+
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        email: "",
+        password: "",
+    })
+
+
+    async function handleUserSignUp(e: React.FormEvent) {
+        e.preventDefault();
+
+        const res = await fetch('http://localhost:8000/signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(signUpData)
+        });
+        const data = await res.json();
+
+        console.log('Signup Response: ', data);
+    }
+
+
+
+    const signInForm = (
+        <form className="LoginFormBox">
+            <h2 className="Stylized">Continue the Tale</h2>
+
+            <div className="InputBox-Outer">
+                <label className="Label" htmlFor="login-username">
+                    <span className="Label-Text Outline Stylized" >Username / Email</span>
+                </label>
+
+                <div className="InputBox-Inner"> 
+                    <img src={imagePaths["nameplate"]} alt="A Nameplate" className="Nameplate" />
+                    <div className="Input-Wrapper">
+                        <input type="text" className="Input" id="login-username" placeholder="Username or Email..." /> 
+                        {/* <div className="Password-Icon-Btn"></div> */}
+                    </div>
+                </div>
+            </div>
+
+            <div className="InputBox-Outer">
+                <label className="Label" htmlFor="login-password">
+                    <span className="Label-Text Outline Stylized">Password</span>
+                </label>
+
+                <div className="InputBox-Inner"> 
+                    <img src={imagePaths["nameplate"]} alt="A Nameplate" className="Nameplate" />
+
+                    <div className="Input-Wrapper">
+                        <input 
+                            type={passwordVisibility["login-password"] ? "text" : "password"}
+                            className="Input" id="login-password" placeholder="Password..." />
+                        
+                        <div className="Password-Icon-Btn">
+                            <button type="button" className="" onClick={() => togglePasswordVisibility("login-password")}>
+                                <img src={passwordVisibility["login-password"] 
+                                    ? imagePaths["eye_visible"] : imagePaths["eye_hidden"] } 
+                                    alt="Toggle Password Visibility" />
+                            </button>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="rhombus-outer">
+                <button type="submit" className="rhombus-inner">
+                    <span className="Btn-Text Outline Stylized">SIGN IN</span>
+                </button>
+            </div>            
+        </form>
+    );
+
+
+
+    const signUpForm = (
+        <form className="LoginFormBox" onSubmit={handleUserSignUp}>
+            <h2 className="Stylized">Write a New Story</h2>
+
+            <div className="InputBox-Outer">
+                <label className="Label" htmlFor="signup-username">
+                    <span className="Label-Text Outline Stylized">Username</span>
+                </label>
+                
+                <div className="InputBox-Inner"> 
+                    <img src={imagePaths["nameplate"]} alt="A Nameplate" className="Nameplate" />
+                    
+                    <div className="Input-Wrapper">
+                        <input 
+                            type="text" className="Input" id="signup-username" 
+                            placeholder="Enter Your Name..." value={signUpData.username}
+                            onChange={(e) => setSignUpData({...signUpData, username: e.target.value})} /> 
+                        {/* <div className="Password-Icon-Btn"></div> */}
+                    </div>
+                </div>
+            </div>
+        
+            <div className="InputBox-Outer">
+                <label className="Label" htmlFor="signup-email">
+                    <span className="Label-Text Outline Stylized">Email</span>
+                </label>
+
+                <div className="InputBox-Inner"> 
+                    <img src={imagePaths["nameplate"]} alt="A Nameplate" className="Nameplate" />
+                    <div className="Input-Wrapper">
+                        <input 
+                            type="email" className="Input" id="signup-email" 
+                            placeholder="Enter Your Email..." value={signUpData.email} 
+                            onChange={(e) => setSignUpData({...signUpData, email: e.target.value})}  /> 
+                        {/* <div className="Password-Icon-Btn"></div> */}
+                    </div>
+                </div>
+            </div>
+
+            
+            <div className="InputBox-Outer">
+                <label className="Label" htmlFor="signup-password">
+                    <span className="Label-Text Outline Stylized">Password</span>
+                </label>
+                
+                <div className="InputBox-Inner"> 
+                    <img src={imagePaths["nameplate"]} alt="A Nameplate" className="Nameplate" />
+                    <div className="Input-Wrapper">
+                        
+                        <input 
+                            type={passwordVisibility["signup-password"] ? "text" : "password"}
+                            className="Input" id="signup-password" 
+                            placeholder="Choose a Password..." value={signUpData.password}
+                            onChange={(e) => setSignUpData({...signUpData, password: e.target.value})} />
+                        {/* <div className="Password-Icon-Btn"> */}
+
+                        <button type="button" className="Password-Icon-Btn" onClick={() => togglePasswordVisibility("signup-password")}>
+                            <img src={passwordVisibility["signup-password"] 
+                                ? imagePaths["eye_visible"] : imagePaths["eye_hidden"] } 
+                                alt="Toggle Password Visibility" />
+                        </button>
+                        {/* </div> */}
+
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div className="InputBox-Outer">
+                <label className="Label" htmlFor="signup-password">
+                    <span className="Label-Text Outline Stylized">Confirm Password</span>
+                </label>
+                
+                <div className="InputBox-Inner"> 
+                    <img src={imagePaths["nameplate"]} alt="A Nameplate" className="Nameplate" />
+                    
+                    <div className="Input-Wrapper">
+                        
+                        <input 
+                            type={passwordVisibility["signup-password-confirm"] ? "text" : "password"}
+                            className="Input" id="signup-password-confirm" 
+                            placeholder="Confirm Password..." />
+                        <button type="button" className="Password-Icon-Btn" onClick={() => togglePasswordVisibility("signup-password-confirm")}>
+                            <img src={passwordVisibility["signup-password-confirm"] 
+                                ? imagePaths["eye_visible"] : imagePaths["eye_hidden"] } 
+                                alt="Toggle Password Visibility" />
+                        </button>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="InputBox-Outer">
+                <label className="Label" htmlFor="signup-password">
+                    <span className="Label-Text Outline Stylized">I am a... DM / Player</span>
+                </label>
+            </div>
+
+            <div className="rhombus-outer">
+                <button type="submit" className="rhombus-inner">
+                    <span className="Btn-Text Outline Stylized">SIGN UP</span>
+                </button>
+            </div>
+        </form>
+    )
+
+
+
     return (
         <div className="TabsRoot">
             <Tabs defaultValue="sign-in">
             
+
                 <TabsList className="login">
                     <TabsTrigger className="TabsTrigger" value="sign-in">Sign In</TabsTrigger>
                     <TabsTrigger className="TabsTrigger" value="sign-up">Sign Up</TabsTrigger>
                 </TabsList>
                 
+
                 <TabsContents>
                     <TabsContent className="TabsContent" value="sign-in">
-                        <h2 className="Stylized">Continue the Tale</h2>
-
-                        <div className="InputBox-Outer">
-                            <label className="Label" htmlFor="login-username">
-                                <span className="Label-Text Outline Stylized" >Username / Email</span>
-                            </label>
-
-                            <div className="InputBox-Inner"> 
-                                <img src={imagePaths["nameplate"]} alt="A Nameplate" className="Nameplate" />
-                                <div className="Input-Wrapper">
-                                    <input type="text" className="Input" id="login-username" placeholder={"Username or Email..."} /> 
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="InputBox-Outer">
-                            <label className="Label" htmlFor="login-password">
-                                <span className="Label-Text Outline Stylized">Password</span>
-                            </label>
-
-                            <div className="InputBox-Inner"> 
-                                <img src={imagePaths["nameplate"]} alt="A Nameplate" className="Nameplate" />
-
-                                <div className="Input-Wrapper">
-                                    <input 
-                                        type={passwordVisibility["login-password"] ? "text" : "password"}
-                                        className="Input" id="login-password" placeholder="Password..." />
-                                    <button type="button" className="Password-Icon-Btn" onClick={() => togglePasswordVisibility("login-password")}>
-                                        <img src={passwordVisibility["login-password"] 
-                                            ? imagePaths["eye_visible"] : imagePaths["eye_hidden"] } 
-                                            alt="Toggle Password Visibility" />
-                                    </button>
-                                </div>
-
-                            </div>
-                        </div>
-
-                        <div className="rhombus-outer">
-                            <button type="submit" className="rhombus-inner">
-                                <span className="Btn-Text Outline Stylized">SIGN IN</span>
-                            </button>
-                        </div>
-
+                        {signInForm}     
                     </TabsContent>
                     
 
                     <TabsContent className="TabsContent" value="sign-up">
-                        <h2 className="Stylized">Write a New Story</h2>
-
-                        <div className="InputBox-Outer">
-                            <label className="Label" htmlFor="signup-username">
-                                <span className="Label-Text Outline Stylized">Username</span>
-                            </label>
-                            
-                            <div className="InputBox-Inner"> 
-                                <img src={imagePaths["nameplate"]} alt="A Nameplate" className="Nameplate" />
-                                
-                                <div className="Input-Wrapper">
-                                    <input type="text" className="Input" id="signup-username" placeholder={"Enter Your Name..."} /> 
-                                </div>
-                            </div>
-                        </div>
-                    
-                        <div className="InputBox-Outer">
-                            <label className="Label" htmlFor="signup-email">
-                                <span className="Label-Text Outline Stylized">Email</span>
-                            </label>
-
-                            <div className="InputBox-Inner"> 
-                                <img src={imagePaths["nameplate"]} alt="A Nameplate" className="Nameplate" />
-                                <div className="Input-Wrapper">
-                                    <input type="text" className="Input" id="signup-email" placeholder={"Enter Your Email..."} /> 
-                                </div>
-                            </div>
-                        </div>
-
-                        
-                        <div className="InputBox-Outer">
-                            <label className="Label" htmlFor="signup-password">
-                                <span className="Label-Text Outline Stylized">Password</span>
-                            </label>
-                            
-                            <div className="InputBox-Inner"> 
-                                <img src={imagePaths["nameplate"]} alt="A Nameplate" className="Nameplate" />
-                                <div className="Input-Wrapper">
-                                    {/* <input type="password" className="Input" id="signup-password" placeholder={"Choose a Password..."}></input> */}
-                                    
-                                    <input 
-                                        type={passwordVisibility["signup-password"] ? "text" : "password"}
-                                        className="Input" id="signup-password" placeholder="Choose a Password..." />
-                                    <button type="button" className="Password-Icon-Btn" onClick={() => togglePasswordVisibility("signup-password")}>
-                                        <img src={passwordVisibility["signup-password"] 
-                                            ? imagePaths["eye_visible"] : imagePaths["eye_hidden"] } 
-                                            alt="Toggle Password Visibility" />
-                                    </button>
-
-                                </div>
-                            </div>
-                        </div>
-                        
-                        
-                        <div className="InputBox-Outer">
-                            <label className="Label" htmlFor="signup-password">
-                                <span className="Label-Text Outline Stylized">Confirm Password</span>
-                            </label>
-                            
-                            <div className="InputBox-Inner"> 
-                                <img src={imagePaths["nameplate"]} alt="A Nameplate" className="Nameplate" />
-                                
-                                <div className="Input-Wrapper">
-                                    {/* <input type="password" className="Input" id="signup-password-confirm" placeholder={"Confirm Password..."}></input> */}
-                                    
-                                    <input 
-                                        type={passwordVisibility["signup-password-confirm"] ? "text" : "password"}
-                                        className="Input" id="signup-password-confirm" placeholder="Confirm Password..." />
-                                    <button type="button" className="Password-Icon-Btn" onClick={() => togglePasswordVisibility("signup-password-confirm")}>
-                                        <img src={passwordVisibility["signup-password-confirm"] 
-                                            ? imagePaths["eye_visible"] : imagePaths["eye_hidden"] } 
-                                            alt="Toggle Password Visibility" />
-                                    </button>
-
-                                </div>
-
-                            </div>
-                        </div>
-
-                        <div className="rhombus-outer">
-                            <button type="submit" className="rhombus-inner">
-                                <span className="Btn-Text Outline Stylized">SIGN UP</span>
-                            </button>
-                        </div>
-                        
+                        {signUpForm}
                     </TabsContent>
                 </TabsContents>
 
